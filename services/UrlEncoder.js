fe1c4f6b3327a9400572e1470db28cf65c394b53
Leo Le <leolemain@gmail.com>
2020-12-09 18:23:17 -0800

    Clean up

--- services/UrlEncoder.js
+++ services/UrlEncoder.js
@@ -1,7 +1,7 @@
 const { customAlphabet } = require('nanoid');
 
 const GenerateUrlCode = () => {
-    const nanoid = customAlphabet('1234567890abcdefQWERASDFZXCVYYUIOHJKLBNM!@$%#&', 5)
+    const nanoid = customAlphabet('1234567890abcdefQWERASDFZXCVYYUIOHJKLBNM', 5)
     return nanoid();
 }
 
